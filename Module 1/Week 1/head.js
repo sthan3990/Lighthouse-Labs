4004a532e009b22c719641cb192b034b3d31e21d
phankons <stephen.han@gmx.com>
2023-04-19 14:14:01 -0400

    test

---
+++ Module 1/Week 1/head.js
@@ -0,0 +1,25 @@
+/* 
+Output:  Create a function head which returns the first item in the array.
+*/
+
+// Load the full build.
+var _ = require('lodash');
+
+
+const assertEqual = function(values, expected) {
+  console.log(_head(values));
+
+  if (actual === expected) {
+    console.log("🛑🛑🛑 Assertion Passed: Actual " + actual + " ==== " + " Expected " + expected );
+  }
+
+  // Otherwise it should print (console.log) the following: Assertion Failed: 
+  // [actual] !== [expected] (but with the values filled in)
+  else {
+    console.log("✅✅✅ Assertion Failed: Actual " + actual + " != " + " Expected " + expected );
+  }
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

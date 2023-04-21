ef864bd85916ef1ab0299d6fb7a34884890a30de
phankons <stephen.han@gmx.com>
2023-04-21 11:01:29 -0400

    Mod1Week1

---
+++ Module 1/Week 1/joinlist.js
@@ -0,0 +1,11 @@
+/* Take a array of strings and concatenate its contents together, with each list item separated from the previous one by a comma. */
+
+const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
+
+function joinList(values) {
+
+  
+
+}
+
+joinList(conceptList);

f5dfb8ca4e52c7797a4411ab1da80d8c890bce72
phankons <stephen.han@gmx.com>
2023-05-23 11:09:25 -0400

    Input Module

--- Module 2/week 1/snake/play.js
+++ Module 2/week 1/snake/play.js
@@ -1,15 +1,13 @@
-
-
 // import Node's net lib 
 const net = require('net');
+const { setupInput } = require('./input');
+const { connect } = require('./client');
 
 //const { Game } = require('./src/Game')
 //const { UserInterface } = require('./src/UserInterface')
 //const { RemoteInterface } = require('./src/RemoteInterface')
 // const game = new Game(new UserInterface(), new RemoteInterface())
 
-const { setupInput } = require('./input');
-const { connect } = require('./client');
 
 console.log("Connecting ...");
 
